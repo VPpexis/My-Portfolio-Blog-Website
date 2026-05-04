fd8d081800ce5229ed69120ee79167ca2a90c3d6
VPpexis <panuganvanphilip@gmail.com>
2026-05-04 14:11:40 +0800

    Testing for Husky

---
+++ components/ui/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from '@/components/ui/navigation-menu'
+
+export function Navbar() {
+    return (
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <div className="container flex h-14 items-center justify-between">
+                
+            </div>
+        </header>
+    )
+}
